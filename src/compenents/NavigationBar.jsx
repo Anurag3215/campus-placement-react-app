ba20709a4ecb1d49eb7c8bfd42f14deb3bac07ef
Anurag3215 <anuragaacc@gmail.com>
2026-07-11 12:45:39 +0530

    NavigationBar updated

---
+++ src/compenents/NavigationBar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const NavigationBar = () => {
+    return (
+        <div>import {useState} from 'react'
+            import reactLogo from './assets/react.svg'
+            import viteLogo from './assets/vite.svg'
+            import heroImg from './assets/hero.png'
+            import './App.css'
+            import {BrowserRouter, Route, Routes} from 'react-router-dom'
+            import AddRegistration from './components/AddRegistration'
+            import SearchRegistration from './components/SearchRegistration'
+            import DeleteRegistration from './components/DeleteRegistration'
+            import ViewRegistration from './components/ViewRegistration'
+
+            function App() {
+  const [count, setCount] = useState(0)
+
+            return (
+            <>
+                <BrowserRouter>
+
+                    <Routes>
+
+                        <Route path='/' element={<AddRegistration />} />
+                        <Route path='/search' element={<SearchRegistration />} />
+                        <Route path='/delete' element={<DeleteRegistration />} />
+                        <Route path='/view' element={<ViewRegistration />} />
+
+                    </Routes>
+
+                </BrowserRouter>
+            </>
+            )
+}
+
+            export default App
+        </div>
+    )
+}
+
+export default NavigationBar

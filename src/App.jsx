b643f2169c1e3604ab43a8551b0034474c1b4802
Anurag3215 <anuragaacc@gmail.com>
2026-07-11 12:06:35 +0530

    SearchStudent Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddStudent from './compenents/AddStudent'
+import SearchRegistration from './compenents/SearchRegistration'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,83 +12,9 @@ function App() {
   return (
     <>
       
-<div className="container">
-                <div className="row">
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
-                        <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-                                <label htmlFor="" className="form-label">Registration ID: </label>
-                                <input type="text" className="form-control" />
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-                                <label htmlFor="" className="form-label">Full Name: </label>
-                                <input type="text" className="form-control" />
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-                                <label htmlFor="" className="form-label">Aadhar Number: </label>
-                                <input type="text" className="form-control" />
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-                                <label htmlFor="" className="form-label">Email Address: </label>
-                                <input type="text" className="form-control" />
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-                                <label htmlFor="" className="form-label">Phone Number: </label>
-                                <input type="text" className="form-control" />
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-                                <label htmlFor="" className="form-label">Brach/Department: </label>
-                                <input type="text" className="form-control" />
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-                                <label htmlFor="" className="form-label">SSLC Mark(%): </label>
-                                <input type="text" className="form-control" />
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-                                <label htmlFor="" className="form-label">Plus Two Mark(%): </label>
-                                <input type="text" className="form-control" />
-
-                            </div>
-
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-                                <label htmlFor="" className="form-label">UG Degree Name and Mark(%): </label>
-                                <input type="text" className="form-control" />
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-
-                                <label htmlFor="" className="form-label">PG Degree Name and Mark(%): </label>
-                                <input type="text" className="form-control" />
-
-                            </div>
-                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-
-                                <button className="btn btn-success">Submit</button>
-
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
+<AddStudent/>
+<SearchRegistration/>
+      
     </>
   )
 }
